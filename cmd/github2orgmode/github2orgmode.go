0fe63b6a9256aa43b2157913e48740ff5d2c24c6
Christian Richter <crichter@suse.com>
2021-08-23 15:31:24 +0200

    Add github skel
    Signed-off-by: Christian Richter <crichter@suse.com>

--- cmd/github2orgmode/github2orgmode.go
+++ cmd/github2orgmode/github2orgmode.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/google/go-github/v38/github"
+	"golang.org/x/oauth2"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	token := os.Getenv("GH_TOKEN")
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(repos)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/dragonchaser/git2orgmode
 
 go 1.16
+
+require (
+	github.com/google/go-github/v38 v38.1.0
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+)
